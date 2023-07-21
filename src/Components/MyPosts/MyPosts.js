b371dcafe9299bff970aa6e9a53f27eb4d06771e
Osorabrian <osorabrian@gmail.com>
2023-07-21 11:58:36 +0300

    edit my posts page

--- src/Components/MyPosts/MyPosts.js
+++ src/Components/MyPosts/MyPosts.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Postcard from '../Feed/Postcard'
+import '../Feed/Feed.css'
 import { useDataStore } from '../../State/state'
 
 export const MyPosts = () => {
 
     const user = useDataStore((state) => state.user)
-    console.log(user)
+    const [search, setSearch] = useState('')
+    const [posts, setPosts] = useState([])
+    
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(posts => setPosts(posts))
+    },[])
 
+    const myPosts = posts.filter((post) => post.userId === user.id)
+    
+    const searchPost = myPosts.filter((post) => post.title.toLowerCase().includes(search.toLowerCase())).map((post) => {
+        return(
+            <Postcard key={post.id} title={post.title} body={post.body}/>
+        )
+    })
+    
   return (
     <div>
         <h1>My Posts</h1>
-
+        <input type={'text'} placeholder='Search My Posts...' id='search-bar' onChange={(e) => {setSearch(e.target.value)}}/>
+        {searchPost}
     </div>
   )
 }
