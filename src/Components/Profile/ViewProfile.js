9d0bc70e83f10827bdd1c4f8bff9c1c4d35ffb33
Osorabrian <osorabrian@gmail.com>
2023-07-21 15:33:39 +0300

    edit view profile page

--- src/Components/Profile/ViewProfile.js
+++ src/Components/Profile/ViewProfile.js
@@ -6,6 +6,38 @@ export default function ViewProfile(){
         <>
         <h1>My Profile</h1>
         <i class="fa-solid fa-user-astronaut fa-2xl" style={{color: '#84c71f'}} id='profile-avatar'></i>
+        <hr></hr>
+        <h1>Perosnal Information</h1>
+
+        <div className="row">
+            <label className='col-4'> Name:</label>
+            <p className="col-6">Brian Osora</p>
+        </div>
+
+        <div className="row">
+            <label className='col-4'>User Name:</label>
+            <p className="col-6">Osoch</p>
+        </div>
+        
+        <div className="row">
+            <label className='col-4'>Email:</label>
+            <p className="col-6">osorabrian@gmail.com</p>
+        </div>
+        
+        <div className="row">
+            <label className='col-4'>Phone:</label>
+            <p className="col-6">Brian Osora</p>
+        </div>
+
+        <div className="row">
+            <label className='col-4'>Website:</label>
+            <p className="col-6">Brian Osora</p>
+        </div>
+        
+        <hr></hr>
+        <h1>Address</h1>
+        <hr></hr>
+        <h1>Company</h1>
         </>
     )
 }
